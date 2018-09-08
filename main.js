617886043a4df3329b00fcd559f023f105578788
Andrew Courey <goldenmambasoft@gmail.com>
2018-09-08 08:20:02 -0400

    Almost done with lightbox

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $("#search").on("input", function() {
     listOfFoundImages = [];
     $("img").each(function(index, image) {
         var searchTerm = $("#search").val().toLowerCase();
-        if ($(image).attr("title").toLowerCase().indexOf(searchTerm) > -1) {
+        if (($(image).attr("title").toLowerCase().indexOf(searchTerm) > -1) || (($(image).attr("alt").toLowerCase().indexOf(searchTerm) > -1))) {
             listOfFoundImages.push(image);
             image.style.display = "inline-block";
         } 
@@ -13,5 +13,14 @@ $("#search").on("input", function() {
         }
    
     });
-
 });
+
+var lightbox = $('#images a').simpleLightbox({showCounter:false,captionsData:"alt"});
+$('#images a').on('show.simplelightbox', function () {
+    $("body").css("backgroundColor","black");
+  });
+  
+  $('#images a').on('close.simplelightbox', function () {
+    $("body").css("backgroundColor","white");
+  });
+  
